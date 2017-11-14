a69b3189310f92e44cbfcee2e4e8078b19ba2553
Andrew Longosz <andrzej.longosz@ez.no>
2017-11-14 15:10:10 +0100

    [DI] Created EzPlatformInstallerBundle

---
+++ src/bundle/EzPlatformInstallerBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformInstallerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzPlatformInstallerBundle extends Bundle
+{
+}

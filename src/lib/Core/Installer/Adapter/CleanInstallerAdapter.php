a04d195007b90e9b56bd5c440f3c63da0e6a9794
Andrew Longosz <andrzej.longosz@ez.no>
2017-11-22 17:28:22 +0100

    [WIP][Core] Implemented clean Installer Adapter

---
+++ src/lib/Core/Installer/Adapter/CleanInstallerAdapter.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformInstaller\Core\Installer\Adapter;
+
+use EzSystems\EzPlatformInstaller\SPI\Installer\Adapter as InstallerAdapter;
+use EzSystems\EzPlatformInstaller\SPI\Installer\Adapter\BaseInstallerAdapter;
+
+class CleanInstallerAdapter extends BaseInstallerAdapter implements InstallerAdapter
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName(): string
+    {
+        return 'clean';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(): void
+    {
+        // TODO: Implement install() method.
+    }
+}

a7cc3a050e6c99d1cd2e25c174c5c6d01b5a1550
Andrew Longosz <andrzej.longosz@ez.no>
2017-11-22 17:26:40 +0100

    [SPI] Created base class providing common features for Adapters

---
+++ src/lib/SPI/Installer/Adapter/BaseInstallerAdapter.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformInstaller\SPI\Installer\Adapter;
+
+/**
+ * Base Installer Adapter to be extended by Adapter implementations.
+ */
+abstract class BaseInstallerAdapter
+{
+    public function __construct()
+    {
+    }
+}

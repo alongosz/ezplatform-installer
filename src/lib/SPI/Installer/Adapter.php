37c7cecb52b3449c401f40f69efe34670a900a5c
Andrew Longosz <andrzej.longosz@ez.no>
2017-11-22 17:24:25 +0100

    [SPI] Created extensibility point interface for installer Adapters

---
+++ src/lib/SPI/Installer/Adapter.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformInstaller\SPI\Installer;
+
+/**
+ * Installer adapter.
+ *
+ * Implement it if you need custom installer logic.
+ */
+interface Adapter
+{
+    const NAME_REGEX = '/^[A-Za-z0-9_-]+$/';
+
+    /**
+     * Get Installer Adapter name.
+     * Requirements:
+     * - unique across all available implementations of Adapter
+     * - alphanumeric (with dashes or underscores, without whitespaces) - see NAME_REGEX.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Perform installer operations.
+     */
+    public function install(): void;
+}
